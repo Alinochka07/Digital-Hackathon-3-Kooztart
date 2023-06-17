92c497820a88f5a8ba72c3f1cd5a1bc161153ddf
Nurtimax <maksatbeknurtilek@gmail.com>
2023-06-17 21:38:19 +0600

    finish medicia slice

---
+++ src/images/FullStar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FullStar = (props) => {
+  return (
+    <svg width="33" height="28" viewBox="0 0 33 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M16.5306 0L20.242 10.5614H32.2522L22.5357 17.0887L26.2471 27.6501L16.5306 21.1228L6.81416 27.6501L10.5255 17.0887L0.809066 10.5614H12.8193L16.5306 0Z"
+        fill="#FEE612"
+      />
+    </svg>
+  );
+};
+
+export default FullStar;
