8508be26ffffa9f19efc98c1e2df4d6eb6c77255
Alinochka07 <alina011988@icloud.com>
2023-06-18 12:00:18 +0600

    fix: after merging branches

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Ambulance from './Ambulance/Ambulance';
 import Optician from '../optician';
 import OnlineConsultation from '../OnlineConsultation/OnlineConsultation';
+import ChooseSpecialist from '../ChooseSpecialist/ChooseSpecialist';
 
 const Main = () => {
     return (
         <main className='mainSection'>
             <OnlineConsultation/>
+            <ChooseSpecialist/>
             <Ambulance/>
             <Optician/>
             
