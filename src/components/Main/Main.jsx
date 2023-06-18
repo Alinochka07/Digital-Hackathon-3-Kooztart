8909d506fa397c3bd8c471e4d27c0448b5c5b571
Alinochka07 <alina011988@icloud.com>
2023-06-18 11:30:06 +0600

    merging branches

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Ambulance from './Ambulance/Ambulance';
 import Optician from '../optician';
-import OpticianBar from '../optician/components/optician-bar/OpticianBar';
 import OnlineConsultation from '../OnlineConsultation/OnlineConsultation';
 
 const Main = () => {
     return (
         <main className='mainSection'>
+            
+            <OnlineConsultation/>
             <Ambulance/>
             <Optician/>
-            <OnlineConsultation/>
+            
         </main>
     );
 };
